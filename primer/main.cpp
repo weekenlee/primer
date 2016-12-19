d94b4643f1bba6323fdb9b7d942aeb56114f12e5
lwj1396@163.com <lwj1396@163.com>
2016-12-19 22:38:45 +0800

    Initial Commit

---
+++ primer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  primer
+//
+//  Created by liweijian on 2016/12/19.
+//  Copyright © 2016年 liweijian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
