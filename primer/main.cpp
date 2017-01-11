a05a13a28a387b77f86109fec1c24a21d47fe921
lwj1396@163.com <lwj1396@163.com>
2017-01-11 13:03:40 +0800

    share pointer

--- primer/main.cpp
+++ primer/main.cpp
@@ -16,6 +16,7 @@
 #include <numeric>
 #include <list>
 #include <iterator>
+#include <exception>
 
 using std::cout;
 using std::cin;
@@ -126,6 +127,48 @@ constexpr int bufsize()
 {
     return 1024;
 }
+
+
+
+
+class StrBlob
+{
+public:
+    typedef std::vector<std::string>::size_type size_type;
+    StrBlob();
+    StrBlob(std::initializer_list<std::string> il);
+    size_type size() const { return data->size(); }
+    bool empty() const { return data->empty(); }
+    // add and remove elements
+    void push_back(const std::string &t) {data->push_back(t);}
+    void pop_back();
+    // element access
+    std::string& front();
+    std::string& back();
+private:
+    std::shared_ptr<std::vector<std::string>> data;
+    // throws msg if data[i] isn't valid
+    void check(size_type i, const std::string &msg) const;
+};
+
+StrBlob::StrBlob(): data(std::make_shared<vector<string>>()) { }
+StrBlob::StrBlob(std::initializer_list<string> il):
+data(std::make_shared<vector<string>>(il)) { }
+
+void StrBlob::check(size_type i, const string &msg) const
+{
+    if (i >= data->size())
+        throw std::out_of_range(msg);
+}
+
+void StrBlob::pop_back()
+{
+    check(0, "pop_back on empty StrBlob");
+    data->pop_back();
+}
+
+
+
 
 int main(int argc, const char * argv[]) {
     
@@ -326,6 +369,12 @@ int main(int argc, const char * argv[]) {
     PRINTLIST
     
     
+    
+    StrBlob v1;
+    {
+        StrBlob v2{"a","2","3"};
+        v1 = v2;
+    }
     
     return 0;
     
