f9e16f36ff508a66199d122894f8d30226b9ccf7
lwj1396@163.com <lwj1396@163.com>
2017-04-23 23:58:33 +0800

    queryText

--- primer/TextQuery.hpp
+++ primer/TextQuery.hpp
@@ -12,10 +12,17 @@
 #include <vector>
 #include <utility>
 #include <unordered_map>
+#include <fstream>
+#include <istream>
+
+#include "easylogging++.h"
+
 using std::string;
 using std::vector;
 using std::pair;
 using std::unordered_map;
+using std::fstream;
+using std::istringstream;
 
 struct QueryResult {
     int totalTimes_;
@@ -28,12 +35,27 @@ private:
     unordered_map<string,vector<int>> strdict_;     //一个字符串，在哪些行
 public:
     TextQuery(const string& filename);
-    ~TextQuery();
+    ~TextQuery() {};
     QueryResult queryText(const string& text);
 };
 
 TextQuery::TextQuery(const string& filename)
 {
-    
+    fstream f(filename,f.out);
+    if (!f.is_open()) {
+        LOG(ERROR)<<"can't not open file "+filename;
+    }else {
+        string line;
+        int lineno = 1;
+        while (f>>line) {
+            filelines_.push_back(std::make_pair(lineno,line));
+            istringstream ss(line);
+            string word;
+            while (ss>>word) {
+                strdict_[word].push_back(lineno);
+            }
+            lineno++;
+        }
+    }
 }
 #endif /* TextQuery_hpp */
